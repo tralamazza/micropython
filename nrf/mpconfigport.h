c9e27d54d5a54f4d668ed79aaa438f33b7dff268
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-08-15 00:44:35 +0200

    nrf: Adding random module to builtins in mpconfigport.h.

--- nrf/mpconfigport.h
+++ nrf/mpconfigport.h
@@ -195,6 +195,7 @@ extern const struct _mp_obj_module_t mp_module_utime;
 extern const struct _mp_obj_module_t mp_module_uos;
 extern const struct _mp_obj_module_t mp_module_ubluepy;
 extern const struct _mp_obj_module_t music_module;
+extern const struct _mp_obj_module_t random_module;
 
 #if MICROPY_PY_UBLUEPY
 #define UBLUEPY_MODULE                      { MP_ROM_QSTR(MP_QSTR_ubluepy), MP_ROM_PTR(&mp_module_ubluepy) },
@@ -208,6 +209,11 @@ extern const struct _mp_obj_module_t music_module;
 #define MUSIC_MODULE
 #endif
 
+#if MICROPY_PY_HW_RNG
+#define RANDOM_MODULE                        { MP_ROM_QSTR(MP_QSTR_random), MP_ROM_PTR(&random_module) },
+#else
+#define RANDOM_MODULE
+#endif
 
 #if BLUETOOTH_SD
 
@@ -227,6 +233,7 @@ extern const struct _mp_obj_module_t ble_module;
     BLE_MODULE \
     MUSIC_MODULE \
     UBLUEPY_MODULE \
+    RANDOM_MODULE \
 
 
 #else
@@ -237,6 +244,7 @@ extern const struct _mp_obj_module_t ble_module;
     { MP_ROM_QSTR(MP_QSTR_utime), MP_ROM_PTR(&mp_module_utime) }, \
     { MP_ROM_QSTR(MP_QSTR_uos), MP_ROM_PTR(&mp_module_uos) }, \
     MUSIC_MODULE \
+    RANDOM_MODULE \
 
 
 #endif // BLUETOOTH_SD
